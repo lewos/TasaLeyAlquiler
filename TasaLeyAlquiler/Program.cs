afed9c6cdc9a72f54297a9d35f2485371f4d7421
Leo <leo.21@live.com.ar>
2022-01-10 03:10:14 -0300

    aver

--- TasaLeyAlquiler/Program.cs
+++ TasaLeyAlquiler/Program.cs
@@ -9,39 +9,12 @@ namespace TasaLeyAlquiler
         {
             try
             {
-                var config = new ConfigurationBuilder()
-                    .AddUserSecrets<Program>()
-                    .Build();
-
-
-                var prueba = Environment.GetEnvironmentVariable("PRUEBA");
-                if (prueba == null)
-                {
-                    throw new Exception("prueba sigue null");
-                }
-                else 
-                {
-                    Console.WriteLine($"tiene un valor");
-                }
 
                 var gitAuthToken = Environment.GetEnvironmentVariable("GH_TOKEN");
-
-                //var gitAuthToken = config.GetSection("GH_TOKEN");
-                if (gitAuthToken == null) 
-                {
-                    throw new Exception("sigue null");
-                }
-                else
-                {
-                    Console.WriteLine($"tiene un valor 2");
-                }
-
-                var repoId = config.GetSection("GH_REPO_ID");
-                var branchToUpdate = config.GetSection("GH_BRANCH");
-                var fileName = config.GetSection("GH_FILE_NAME");
-
-
-
+             
+                var repoId = Environment.GetEnvironmentVariable("GH_REPO_ID");
+                var branchToUpdate = Environment.GetEnvironmentVariable("GH_BRANCH");
+                var fileName = Environment.GetEnvironmentVariable("GH_FILE_NAME");
 
                 // Fecha de inicio
                 var desde = new DateTime(2020, 07, 01);
@@ -58,14 +31,13 @@ namespace TasaLeyAlquiler
 
                 ////Console.WriteLine($"Request disponibles: {gitHubService.CheckRate()}");
 
-                var repo = await gitHubService.GetRepo(Convert.ToInt64(repoId.Value));
+                var repo = await gitHubService.GetRepo(Convert.ToInt64(repoId));
 
-
-                var tuple = await gitHubService.GetFileContent(Convert.ToInt64(repoId.Value), fileName.Value);
+                var tuple = await gitHubService.GetFileContent(Convert.ToInt64(repoId), fileName);
                 var currentFileText = tuple.Item1;
                 var targetFileSha = tuple.Item2;
 
-                await gitHubService.UpdateAndCommit(Convert.ToInt64(repoId.Value), currentFileText, fileName.Value, dict, branchToUpdate.Value, targetFileSha);
+                await gitHubService.UpdateAndCommit(Convert.ToInt64(repoId), currentFileText, fileName, dict, branchToUpdate, targetFileSha);
 
             }
             catch (Exception ex)
